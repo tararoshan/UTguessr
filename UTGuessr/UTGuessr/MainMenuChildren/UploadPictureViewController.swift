4412d8c540f8fff55c77ac042e5657565d396e78
Alex Lu <alex@Alexs-MacBook-Pro.local>
2023-11-05 10:58:12 -0600

    working on upload image

---
+++ UTGuessr/UTGuessr/MainMenuChildren/UploadPictureViewController.swift
@@ -0,0 +1,51 @@
+//
+//  UploadPictureViewController.swift
+//  UTGuessr
+//
+//  Created by Alex Lu on 11/4/23.
+//
+
+import UIKit
+
+class UploadPictureViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+
+    @IBOutlet weak var takePhotoButton: UIButton!
+    @IBOutlet weak var uploadPhotoButton: UIButton!
+    var imagePicker = UIImagePickerController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func takePhotoPressed(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+                    print("Button capture")
+
+                    imagePicker.delegate = self
+                    imagePicker.sourceType = .savedPhotosAlbum // TOOD: CHANGE TO .camera in final. using album for debug purposes
+                    imagePicker.allowsEditing = false
+
+                    present(imagePicker, animated: true, completion: nil)
+            }
+    }
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+            self.dismiss(animated: true, completion: { () -> Void in
+
+            })
+
+            imageView.image = image
+    }
+    @IBAction func uploadPhotoPressed(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
